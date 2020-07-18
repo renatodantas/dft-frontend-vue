b170d96d7eecea66f43ea2c2cc3929ec7aa268fd
Renato Dantas <renato.dantas@tst.jus.br>
2020-07-18 12:33:03 -0300

    Atualizando nome do projeto

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,7 @@
   <div class="jumbotron">
     <h1 class="display-4">Página inicial do DFT</h1>
     <p class="lead">
-      Bem vindo ao Dimensionamento da Força de Trabalho.
+      Bem vindo ao Dignus Fortis Thesaurus.
     </p>
     <hr class="my-4" />
     <p>Para iniciar o cadastro e os preenchimentos dos esforços, utilize o menu superior.</p>
